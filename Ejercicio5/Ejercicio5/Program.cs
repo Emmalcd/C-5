e94b542ef6f9827f1686ad72b78907dc7fad2e60
Emmalcd <joselemmanuel9@gmail.com>
2024-01-20 15:16:02 -0600

    Ejercicio 5

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.WriteLine("Hola, soy Emma");
+            Console.ReadLine();
+        }
+    }
+}
